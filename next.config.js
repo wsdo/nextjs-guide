fefb21f239950c3197e9d364bc9335e01df1c417
starkwang <wsd312@163.com>
2019-12-03 11:11:38 +0800

    feat: 删除配置文件

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const withCSS = require('@zeit/next-css')
-const dev = process.env.NODE_ENV != 'production'
-const localIdentName = dev ? '[local]-[hash:base64:5]' : '[hash:base64:5]'
 module.exports = withCSS({
   cssModules: true,
   cssLoaderOptions: {
